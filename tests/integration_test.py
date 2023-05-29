549bb0e5c2548763935559037289e1fee7aa708a
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-05-29 16:06:54 +0100

    try this

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,46 +1,45 @@
 import filecmp
 import subprocess
-
+import os
+import json
 
 def test_help():
     result = subprocess.run(["terravision", "--help"], stdout=subprocess.PIPE)
     assert "Terravision" in result.stdout.decode("utf-8") and result.returncode == 0
 
 
-def test_wordpress_fargate():
+def verify_json_output(result_file, github_repo, var_file):
+    output_file = 'result.json'
     result = subprocess.run(
         [
-            "terravision",
-            "graphdata",
-            "--source",
-            "https://github.com/futurice/terraform-examples.git//aws/wordpress_fargate",
-            "--varfile",
-            "examples/variables.tfvars",
-            "--outfile",
-            "tests/architecture-wordpress.json",
+            "terravision", "graphdata",
+            "--source", github_repo,
+            "--varfile", var_file,
+            "--outfile", output_file,
         ],
         stdout=subprocess.PIPE,
     )
-    assert (
-        filecmp.cmp(
-            "tests/architecture-wordpress.json", "tests/testcase-wordpress.json"
-        )
-        and result.returncode == 0
-    )
+    assert result.returncode == 0
+    assert os.path.isfile(output_file)
+    with open(output_file) as json_file:
+        result = json.load(json_file)
+
+    print('----- result')
+    print(result)
+    with open(result_file) as json_file:
+        expected = json.load(json_file)
+    assert result == expected
 
 
-# def test_static_site():
-#     result = subprocess.run(
-#         [
-#             "terravision",
-#             "graphdata",
-#             "--source",
-#             "https://github.com/futurice/terraform-examples.git//aws/aws_static_site",
-#             "--varfile",
-#             "examples/variables.tfvars",
-#             "--outfile",
-#             "tests/architecture-static-site.json",
-#         ],
-#         stdout=subprocess.PIPE,
-#     )
-#     assert filecmp.cmp("tests/architecture-static-site.json", "tests/testcase-wordpress.json" )and result.returncode == 0
+def test_wordpress_fargate():
+    expected_file = f"{os.getcwd()}/tests/testcase-wordpress.json"
+    github_repo = "https://github.com/futurice/terraform-examples.git//aws/wordpress_fargate"
+    var_file = "examples/variables.tfvars"
+    verify_json_output(expected_file, github_repo, var_file)
+
+
+def test_static_site():
+    expected_file = f"{os.getcwd()}/tests/testcase-aws-static-site.json"
+    github_repo = "https://github.com/futurice/terraform-examples.git//aws/aws_static_site"
+    var_file = "examples/variables.tfvars"
+    verify_json_output(expected_file, github_repo, var_file)
