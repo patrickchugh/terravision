dcfb3c356e413bf8a9a1600e108d24a3424061aa
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-06-02 15:08:48 +0100

    fix #35

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -3,11 +3,11 @@ import os
 import json
 import pathlib
 
+
 def test_help():
     result = subprocess.run(["terravision", "--help"], stdout=subprocess.PIPE)
     assert "Terravision" in result.stdout.decode("utf-8") and result.returncode == 0
 
-base_repo = "https://github.com/patrickchugh/terraform-examples.git"
 
 def verify_json_output(github_repo, var_file, expected_output):
     output_file = f'{os.getcwd()}/output/result.json'
@@ -27,7 +27,7 @@ def verify_json_output(github_repo, var_file, expected_output):
     assert os.path.isfile(output_file)
     with open(output_file) as json_file:
         result = json.load(json_file)
-    with open(expected_output) as json_file:
+    with open(f"{os.getcwd()}/tests/expected_outputs/{expected_output}") as json_file:
         expected = json.load(json_file)
     assert result == expected
 
@@ -38,9 +38,23 @@ def verify_json_output(github_repo, var_file, expected_output):
 #     expected_output = f"{os.getcwd()}/tests/testcase-wordpress.json"
 #     verify_json_output(github_repo, var_file, expected_output)
 
+var_file = "examples/variables.tfvars"
+base_repo = "https://github.com/patrickchugh/terraform-examples.git"
+
 
 def test_static_site():
     github_repo = f"{base_repo}//aws/aws_static_site"
-    var_file = "examples/variables.tfvars"
-    expected_output = f"{os.getcwd()}/tests/testcase-aws-static-site.json"
+    expected_output = "aws-static-site.json"
+    verify_json_output(github_repo, var_file, expected_output)
+
+
+def test_aws_domain_redirect():
+    github_repo = f"{base_repo}//aws/aws_domain_redirect"
+    expected_output = "aws-domain-redirect.json"
+    verify_json_output(github_repo, var_file, expected_output)
+
+
+def test_aws_lambda_api():
+    github_repo = f"{base_repo}//aws/aws_lambda_api"
+    expected_output = "aws-lambda-api.json"
     verify_json_output(github_repo, var_file, expected_output)
