e24de12c703ddce7d54c6e7a281e38fb1e632944
Patrick Chugh <99465091+patrickchugh@users.noreply.github.com>
2023-08-22 10:00:33 +0100

    Update gitlibs.py

--- modules/gitlibs.py
+++ modules/gitlibs.py
@@ -54,9 +54,9 @@ def get_clone_url(sourceURL: str):
     subfolder = ""
     # Handle Case where full git url is given
     if (
-        "github.com" in sourceURL
-        or "gitlab.com" in sourceURL
-        or sourceURL.startswith("https://git")
+        ("github.com" in sourceURL
+        or "gitlab.com" in sourceURL)
+        and sourceURL.startswith("https://git")
     ):
         gitaddress = ""
         subfolder = ""
