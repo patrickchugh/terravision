241d008acf17f4b277b45dbb2a689a0c26d9cad0
patrickchugh <me@patrickchugh.com>
2025-11-23 09:34:24 +0000

    Removed walklevel test

--- tests/fileparser_unit_test.py
+++ tests/fileparser_unit_test.py
@@ -6,26 +6,8 @@ parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir)
 
 from modules.fileparser import (
-    walklevel,
     handle_module,
 )
-
-
-class TestWalklevel(unittest.TestCase):
-    def test_walklevel_basic(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            test_file = os.path.join(tmpdir, "test.tf")
-            with open(test_file, "w") as f:
-                f.write("# test")
-            result = list(walklevel(tmpdir, level=1))
-            self.assertGreater(len(result), 0)
-
-    def test_walklevel_depth_limit(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            subdir = os.path.join(tmpdir, "sub1", "sub2")
-            os.makedirs(subdir)
-            result = list(walklevel(tmpdir, level=1))
-            self.assertIsInstance(result, list)
 
 
 class TestHandleModule(unittest.TestCase):
